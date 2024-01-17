96076640feaac2cf45c44b898bb6b1074722e744
Ramirezi23 <nabaisnelson@gmail.com>
2024-01-17 11:23:44 +0000

    Criei builder em creational

---
+++ src/creational/builder/classes/vegan-dish-builder.ts
@@ -0,0 +1,39 @@
+import { MealBuilderProtocol } from '../interfaces/meal-builder-protocol';
+import { MealBox } from './meal-box';
+import { Beans, Beverage, Dessert, Rice } from './meals';
+
+export class VeganDishBuilder implements MealBuilderProtocol {
+  private _meal: MealBox = new MealBox();
+
+  reset(): this {
+    this._meal = new MealBox();
+    return this;
+  }
+
+  makeMeal(): this {
+    const rice = new Rice('Arroz', 5);
+    const beans = new Beans('Feijão', 10);
+    this._meal.add(rice, beans);
+    return this;
+  }
+
+  makeBeverage(): this {
+    const beverage = new Beverage('Coca-Cola', 3);
+    this._meal.add(beverage);
+    return this;
+  }
+
+  makeDessert(): this {
+    const dessert = new Dessert('Gelado', 5);
+    this._meal.add(dessert);
+    return this;
+  }
+
+  getMeal(): MealBox {
+    return this._meal;
+  }
+
+  getPrice(): number {
+    return this._meal.getPrice();
+  }
+}
